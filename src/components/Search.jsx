70dbe86881d681c35dc79a86198bc96b73bafc8a
JokyOFC <personal_joky@outlook.com>
2023-04-15 03:02:36 -0300

    initial commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Search = () => {
+    return (
+        <div className="search">
+            <div className="searchForm">
+                <input type="text"/>
+            </div>
+            <div className="userChat">
+                <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg" alt="" placeholder="Find a user"/>
+                <div className="userChatInfo">
+                    <span>Kauan</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
